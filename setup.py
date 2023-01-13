6e8b6c1d97faf350bb36eaef7c11699b92c15cc2
Mohammad-SS <mohammad.azimi.1996@gmail.com>
2023-01-13 12:20:50 +0330

    initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='django-fakeit',
+    packages=find_packages(include=['django-fakeit']),
+    version='0.1.0',
+    description='its a django module to create dummy data based on models in django',
+    author='MAzimi',
+    license='MIT',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests',
+)
